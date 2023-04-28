ab8255ccd42c2c1ab4e4974d44869931b79e566b
misha <mikhail.kutovenko@gmail.com>
2023-04-28 23:28:45 +0300

    Add BaseIntegrationTest

---
+++ resource-server/src/test/java/ru/clevertec/newsresource/integration/BaseIntegrationTest.java
@@ -0,0 +1,32 @@
+package ru.clevertec.newsresource.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(value = "classpath:db/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public abstract class BaseIntegrationTest {
+
+    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:13.3");
+
+    @BeforeAll
+    static void init() {
+        POSTGRES_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void setup(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+    }
+
+}
