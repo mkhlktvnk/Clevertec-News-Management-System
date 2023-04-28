42a91d553f2186cee2e7c78d8f1e429f6b5e407d
misha <mikhail.kutovenko@gmail.com>
2023-04-29 00:13:39 +0300

    Refactor with param 'name'

--- resource-server/src/main/java/ru/clevertec/newsresource/cache/impl/LRUCache.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/cache/impl/LRUCache.java
@@ -9,34 +9,38 @@ import java.util.Map;
 public class LRUCache implements Cache<String, Object> {
     private final int MAX_ENTRIES;
     private final Map<String, Object> cache;
-    private final LinkedList<String> keys;
+    private final Map<String, LinkedList<String>> keys;
 
     public LRUCache(Integer capacity) {
         this.cache = new HashMap<>(capacity);
-        this.keys = new LinkedList<>();
+        this.keys = new HashMap<>();
         this.MAX_ENTRIES = capacity;
     }
 
     @Override
-    public void put(String key, Object value) {
-        if (cache.containsKey(key)) {
-            keys.remove(key);
+    public void put(String key, String name, Object value) {
+        String cacheKey = name + ":" + key;
+        if (cache.containsKey(cacheKey)) {
+            keys.get(name).remove(key);
         }
-        cache.put(key, value);
-        keys.addFirst(key);
-        if (keys.size() > MAX_ENTRIES) {
-            String last = keys.removeLast();
-            cache.remove(last);
+        cache.put(cacheKey, value);
+        keys.putIfAbsent(name, new LinkedList<>());
+        keys.get(name).addFirst(key);
+        if (keys.get(name).size() > MAX_ENTRIES) {
+            String last = keys.get(name).removeLast();
+            cache.remove(name + ":" + last);
         }
     }
 
     @Override
-    public Object get(String key) {
-        Object value = cache.get(key);
+    public Object get(String key, String name) {
+        String cacheKey = name + ":" + key;
+        Object value = cache.get(cacheKey);
         if (value != null) {
-            keys.remove(key);
-            keys.addFirst(key);
+            keys.get(name).remove(key);
+            keys.get(name).addFirst(key);
         }
         return value;
     }
+
 }
