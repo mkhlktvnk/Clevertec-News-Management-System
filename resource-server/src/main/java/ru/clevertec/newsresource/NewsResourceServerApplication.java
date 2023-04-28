b36622d161de2020d60c2c28997e83bfc41dc187
misha <mikhail.kutovenko@gmail.com>
2023-04-28 16:40:51 +0300

    Add NewsResourceServerApplication

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/NewsResourceServerApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.newsresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsResourceServerApplication.class);
+    }
+
+}
