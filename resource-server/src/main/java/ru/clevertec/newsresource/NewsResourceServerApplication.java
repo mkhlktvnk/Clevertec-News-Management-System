33f152214b776e1339f97c6b9fa5c4965fad3caa
misha <mikhail.kutovenko@gmail.com>
2023-04-29 01:18:36 +0300

    Add @ConfigurationPropertiesScan

--- resource-server/src/main/java/ru/clevertec/newsresource/NewsResourceServerApplication.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/NewsResourceServerApplication.java
@@ -2,8 +2,10 @@ package ru.clevertec.newsresource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("ru.clevertec.newsresource")
 public class NewsResourceServerApplication {
 
     public static void main(String[] args) {
