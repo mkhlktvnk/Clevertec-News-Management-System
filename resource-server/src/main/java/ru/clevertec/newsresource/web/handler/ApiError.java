5a9744de5d1e310e1a6292c35db5308a34ec37fa
misha <mikhail.kutovenko@gmail.com>
2023-04-28 23:14:55 +0300

    Refactor imports

--- resource-server/src/main/java/ru/clevertec/newsresource/web/handler/ApiError.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/web/handler/ApiError.java
@@ -1,6 +1,10 @@
 package ru.clevertec.newsresource.web.handler;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
