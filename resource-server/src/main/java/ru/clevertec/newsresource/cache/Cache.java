ffc064f2da2ea9747dca9dff4ef4a1eea11954f7
misha <mikhail.kutovenko@gmail.com>
2023-04-29 00:11:38 +0300

    Add param 'name' to avoid collisions

--- resource-server/src/main/java/ru/clevertec/newsresource/cache/Cache.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/cache/Cache.java
@@ -1,7 +1,7 @@
 package ru.clevertec.newsresource.cache;
 
 public interface Cache<K, V> {
-    void put(K key, V value);
+    void put(K key, String name, V value);
 
-    V get(K key);
+    V get(K key, String name);
 }
