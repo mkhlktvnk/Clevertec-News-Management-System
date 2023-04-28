820c80619de31c19444bec56bf97dc8ad4a5b5d9
misha <mikhail.kutovenko@gmail.com>
2023-04-28 19:23:15 +0300

    Add NewsTestBuilder

---
+++ resource-server/src/test/java/ru/clevertec/newsresource/builder/impl/NewsTestBuilder.java
@@ -0,0 +1,36 @@
+package ru.clevertec.newsresource.builder.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import ru.clevertec.newsresource.builder.TestBuilder;
+import ru.clevertec.newsresource.entity.Comment;
+import ru.clevertec.newsresource.entity.News;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@With
+@AllArgsConstructor
+@NoArgsConstructor(staticName = "aNews")
+public class NewsTestBuilder implements TestBuilder<News> {
+    private Long id = 0L;
+    private Instant time = Instant.now();
+    private String title = "";
+    private String text = "";
+    private List<Comment> comments = new ArrayList<>();
+
+    @Override
+    public News build() {
+        News news = new News();
+
+        news.setId(id);
+        news.setTime(time);
+        news.setTitle(title);
+        news.setTitle(text);
+        news.setComments(comments);
+
+        return news;
+    }
+}
