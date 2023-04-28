9a80b751645ca9f8d961bd0dd68fc185afc1d0d7
misha <mikhail.kutovenko@gmail.com>
2023-04-28 20:01:35 +0300

    Add CommentService

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/CommentService.java
@@ -0,0 +1,19 @@
+package ru.clevertec.newsresource.service;
+
+import org.springframework.data.domain.Pageable;
+import ru.clevertec.newsresource.criteria.CommentCriteria;
+import ru.clevertec.newsresource.entity.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+    List<Comment> findAllByPageableAndCriteria(Pageable pageable, CommentCriteria criteria);
+
+    Comment findAllById(Long commentId);
+
+    Comment insertComment(Comment comment);
+
+    void updateCommentPartiallyById(Long commentId, Comment updateComment);
+
+    void deleteCommentById(Long commentId);
+}
