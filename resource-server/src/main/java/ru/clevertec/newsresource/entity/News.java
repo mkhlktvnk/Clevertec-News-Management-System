e31a1a9d3d9b045a74be5c9aba0a4f964f36fd33
misha <mikhail.kutovenko@gmail.com>
2023-04-28 17:32:17 +0300

    Add News entity

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/entity/News.java
@@ -0,0 +1,44 @@
+package ru.clevertec.newsresource.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "news")
+public class News implements BaseEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Instant time;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String text;
+
+    @OneToMany(mappedBy = "news", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+}
