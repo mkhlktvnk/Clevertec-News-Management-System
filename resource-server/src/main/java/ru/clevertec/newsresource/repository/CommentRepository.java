ee386262f910e5376a5651bb0075d65b0ec78b77
misha <mikhail.kutovenko@gmail.com>
2023-04-29 12:24:05 +0300

    Add method 'findAllByNewsId'

--- resource-server/src/main/java/ru/clevertec/newsresource/repository/CommentRepository.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/repository/CommentRepository.java
@@ -1,11 +1,14 @@
 package ru.clevertec.newsresource.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.clevertec.newsresource.entity.Comment;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
-
+    List<Comment> findAllByNewsId(Long newsId, Pageable pageable);
 }
