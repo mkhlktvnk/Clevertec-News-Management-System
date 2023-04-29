51b8f199a4309d5ce6efa47878fcd4540e1bb007
misha <mikhail.kutovenko@gmail.com>
2023-04-29 14:03:25 +0300

    Refactor LFUCache

--- resource-server/src/main/java/ru/clevertec/newsresource/cache/impl/LFUCache.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/cache/impl/LFUCache.java
@@ -6,12 +6,14 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LFUCache implements Cache<String, Object> {
     private final int MAX_ENTRIES;
     private final Map<String, Object> cache;
     private final Map<String, Integer> usageCount;
     private final Map<Integer, LinkedHashSet<String>> frequencyList;
+    private final ReentrantReadWriteLock lock;
     private int minFrequency = 0;
 
     public LFUCache(int capacity) {
@@ -20,47 +22,58 @@ public class LFUCache implements Cache<String, Object> {
         usageCount = new HashMap<>(capacity);
         frequencyList = new HashMap<>();
         frequencyList.put(1, new LinkedHashSet<>());
+        lock = new ReentrantReadWriteLock();
     }
 
     @Override
     public void put(String key, String name, Object value) {
-        String cacheKey = key + ":" + name;
-        if (cache.containsKey(cacheKey)) {
+        lock.writeLock().lock();
+        try {
+            String cacheKey = key + ":" + name;
+            if (cache.containsKey(cacheKey)) {
+                cache.put(cacheKey, value);
+                get(key, name);
+                return;
+            }
+
+            if (cache.size() == MAX_ENTRIES) {
+                String evictionCandidate = frequencyList.get(minFrequency).iterator().next();
+                frequencyList.get(minFrequency).remove(evictionCandidate);
+                cache.remove(evictionCandidate);
+                usageCount.remove(evictionCandidate);
+            }
+
             cache.put(cacheKey, value);
-            get(key, name);
-            return;
+            usageCount.put(cacheKey, 1);
+            frequencyList.get(1).add(cacheKey);
+            minFrequency = 1;
+        } finally {
+            lock.writeLock().unlock();
         }
-
-        if (cache.size() == MAX_ENTRIES) {
-            String evictionCandidate = frequencyList.get(minFrequency).iterator().next();
-            frequencyList.get(minFrequency).remove(evictionCandidate);
-            cache.remove(evictionCandidate);
-            usageCount.remove(evictionCandidate);
-        }
-
-        cache.put(cacheKey, value);
-        usageCount.put(cacheKey, 1);
-        frequencyList.get(1).add(cacheKey);
-        minFrequency = 1;
     }
 
     @Override
     public Optional<Object> get(String key, String name) {
-        String cacheKey = key + ":" + name;
-        if (!cache.containsKey(cacheKey)) {
-            return Optional.empty();
-        }
+        lock.readLock().lock();
+        try {
+            String cacheKey = key + ":" + name;
+            if (!cache.containsKey(cacheKey)) {
+                return Optional.empty();
+            }
 
-        int count = usageCount.get(cacheKey);
-        usageCount.put(cacheKey, count + 1);
-        frequencyList.get(count).remove(cacheKey);
-        if (count == minFrequency && frequencyList.get(count).isEmpty()) {
-            minFrequency++;
-            frequencyList.remove(count);
+            int count = usageCount.get(cacheKey);
+            usageCount.put(cacheKey, count + 1);
+            frequencyList.get(count).remove(cacheKey);
+            if (count == minFrequency && frequencyList.get(count).isEmpty()) {
+                minFrequency++;
+                frequencyList.remove(count);
+            }
+            frequencyList.putIfAbsent(count + 1, new LinkedHashSet<>());
+            frequencyList.get(count + 1).add(cacheKey);
+            return Optional.ofNullable(cache.get(cacheKey));
+        } finally {
+            lock.readLock().unlock();
         }
-        frequencyList.putIfAbsent(count + 1, new LinkedHashSet<>());
-        frequencyList.get(count + 1).add(cacheKey);
-        return Optional.ofNullable(cache.get(cacheKey));
     }
 }
 
