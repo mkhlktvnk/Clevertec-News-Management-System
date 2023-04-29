eaf74d17af7a4b7917ce5af4457eba11db75400f
misha <mikhail.kutovenko@gmail.com>
2023-04-29 13:04:54 +0300

    Add Specification for newsId

--- resource-server/src/main/java/ru/clevertec/newsresource/specifications/CommentSpecifications.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/specifications/CommentSpecifications.java
@@ -7,6 +7,15 @@ import ru.clevertec.newsresource.entity.Comment;
 @UtilityClass
 public class CommentSpecifications {
 
+    public Specification<Comment> hasNewsId(Long newsId) {
+        return ((root, query, criteriaBuilder) -> {
+            if (newsId == null) {
+                return criteriaBuilder.isTrue(criteriaBuilder.literal(true));
+            }
+            return criteriaBuilder.equal(root.get("news"), newsId);
+        });
+    }
+
     public Specification<Comment> hasTextLike(String text) {
         return ((root, query, criteriaBuilder) -> {
             if (text == null) {
