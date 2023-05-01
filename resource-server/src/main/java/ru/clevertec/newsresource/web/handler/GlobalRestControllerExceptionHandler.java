d406171bade76100673bed3b88bc3c77ffbb6356
misha <mikhail.kutovenko@gmail.com>
2023-05-01 19:23:05 +0300

    Change ApiError import

--- resource-server/src/main/java/ru/clevertec/newsresource/web/handler/GlobalRestControllerExceptionHandler.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/web/handler/GlobalRestControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.clevertec.newsresource.service.exception.ResourceNotFoundException;
+import ru.clevertec.newsresource.web.dto.ApiError;
 
 @RestControllerAdvice
 public class GlobalRestControllerExceptionHandler extends ResponseEntityExceptionHandler {
