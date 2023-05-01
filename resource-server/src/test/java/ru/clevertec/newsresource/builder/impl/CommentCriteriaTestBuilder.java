4eb38d55c9d64954527cbcfd91543412fd83599d
misha <mikhail.kutovenko@gmail.com>
2023-05-01 11:58:57 +0300

    Add lombok annotations

--- resource-server/src/test/java/ru/clevertec/newsresource/builder/impl/CommentCriteriaTestBuilder.java
+++ resource-server/src/test/java/ru/clevertec/newsresource/builder/impl/CommentCriteriaTestBuilder.java
@@ -1,8 +1,14 @@
 package ru.clevertec.newsresource.builder.impl;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.With;
 import ru.clevertec.newsresource.builder.TestBuilder;
 import ru.clevertec.newsresource.web.criteria.CommentCriteria;
 
+@With
+@AllArgsConstructor
+@NoArgsConstructor(staticName = "aComment")
 public class CommentCriteriaTestBuilder implements TestBuilder<CommentCriteria> {
     private String text = "";
     private String username = "";
