fcc58563966a030f28f42373c0748951bd982bbd
misha <mikhail.kutovenko@gmail.com>
2023-05-01 15:50:21 +0300

    Add method to map not null fields

--- resource-server/src/main/java/ru/clevertec/newsresource/web/mapper/NewsMapper.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/web/mapper/NewsMapper.java
@@ -1,6 +1,6 @@
 package ru.clevertec.newsresource.web.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 import ru.clevertec.newsresource.entity.News;
 import ru.clevertec.newsresource.web.dto.NewsDto;
 
@@ -14,4 +14,19 @@ public interface NewsMapper {
     News toEntity(NewsDto newsDto);
 
     List<NewsDto> toDto(Collection<News> news);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "time", ignore = true),
+            @Mapping(target = "comments", ignore = true),
+            @Mapping(
+                    target = "title",
+                    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+            ),
+            @Mapping(
+                    target = "text",
+                    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+            )
+    })
+    void mapNotNullFields(@MappingTarget News news, News source);
 }
