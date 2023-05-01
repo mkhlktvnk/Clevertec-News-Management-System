aa6eacfb3bdd02ff65031dcdef685078d5a25a46
misha <mikhail.kutovenko@gmail.com>
2023-05-02 01:15:43 +0300

    Remove unnecessary classes

--- resource-server/src/main/java/ru/clevertec/newsresource/service/impl/CommentServiceImpl.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/impl/CommentServiceImpl.java
@@ -2,8 +2,6 @@ package ru.clevertec.newsresource.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -17,11 +15,10 @@ import ru.clevertec.newsresource.repository.CommentRepository;
 import ru.clevertec.newsresource.repository.NewsRepository;
 import ru.clevertec.newsresource.service.CommentService;
 import ru.clevertec.newsresource.service.exception.ResourceNotFoundException;
-import ru.clevertec.newsresource.service.message.key.CommentMessageKey;
 import ru.clevertec.newsresource.service.message.MessagesSource;
+import ru.clevertec.newsresource.service.message.key.CommentMessageKey;
 import ru.clevertec.newsresource.service.message.key.NewsMessageKey;
 import ru.clevertec.newsresource.specifications.CommentSpecifications;
-import ru.clevertec.newsresource.web.criteria.CommentCriteria;
 import ru.clevertec.newsresource.web.mapper.CommentMapper;
 
 import java.util.List;
