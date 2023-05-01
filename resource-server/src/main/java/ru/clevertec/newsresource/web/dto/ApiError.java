36c26b46cc9be658d0b2600b423d7dd5b1e1656e
misha <mikhail.kutovenko@gmail.com>
2023-05-01 19:19:34 +0300

    Add OpenAPI Docs

--- resource-server/src/main/java/ru/clevertec/newsresource/web/dto/ApiError.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/web/dto/ApiError.java
@@ -1,5 +1,6 @@
 package ru.clevertec.newsresource.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,13 +8,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@Schema(description = "Error information")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ApiError {
+
+    @Schema(description = "Error status")
     private HttpStatus status;
+
+    @Schema(description = "Error message")
     private String message;
+
+    @Schema(description = "The path where the error occurred")
     private String path;
+
 }
