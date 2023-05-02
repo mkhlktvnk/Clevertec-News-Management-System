41b6f814a9a335ed14278de9ea9d14491d6e6de1
misha <mikhail.kutovenko@gmail.com>
2023-05-02 23:34:25 +0300

    Remove ApiError, GlobalRestControllerExceptionHandler, ResourceNotFoundException

--- resource-server/src/main/java/ru/clevertec/newsresource/service/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.clevertec.newsresource.service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
