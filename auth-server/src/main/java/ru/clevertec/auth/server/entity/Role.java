1742b6d769541067d66e81d016f622ed9021544c
misha <mikhail.kutovenko@gmail.com>
2023-05-03 14:08:16 +0300

    Add Role

---
+++ auth-server/src/main/java/ru/clevertec/auth/server/entity/Role.java
@@ -0,0 +1,35 @@
+package ru.clevertec.auth.server.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Collection<User> users;
+
+}
