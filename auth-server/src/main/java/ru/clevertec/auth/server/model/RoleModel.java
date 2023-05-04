aa6eb88ee1431bffcf93954bd2dd8172349dc133
misha <mikhail.kutovenko@gmail.com>
2023-05-04 14:32:03 +0300

    Add RoleModel

---
+++ auth-server/src/main/java/ru/clevertec/auth/server/model/RoleModel.java
@@ -0,0 +1,16 @@
+package ru.clevertec.auth.server.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleModel {
+
+    @JsonProperty(value = "authority", access = JsonProperty.Access.READ_ONLY)
+    private String authority;
+
+}
