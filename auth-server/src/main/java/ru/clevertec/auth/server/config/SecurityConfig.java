ebd995e1b5f342753b6b27d527acf4f4c4ad2755
misha <mikhail.kutovenko@gmail.com>
2023-05-04 14:30:38 +0300

    Add SecurityConfig

---
+++ auth-server/src/main/java/ru/clevertec/auth/server/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package ru.clevertec.auth.server.config;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .build();
+    }
+
+    @Bean
+    @SneakyThrows
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http, UserDetailsService service, BCryptPasswordEncoder encoder) {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(service)
+                .passwordEncoder(encoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
