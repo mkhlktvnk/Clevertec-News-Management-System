92d772d9d4c97579aaf25c270a3b05201d981d28
misha <mikhail.kutovenko@gmail.com>
2023-05-04 14:40:22 +0300

    Add AuthenticationException handler

--- exception-handling-starter/src/main/java/ru/clevertec/exception/handling/starter/handler/GlobalExceptionHandler.java
+++ exception-handling-starter/src/main/java/ru/clevertec/exception/handling/starter/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.clevertec.exception.handling.starter.exception.AuthenticationException;
 import ru.clevertec.exception.handling.starter.exception.ResourceNotFoundException;
 import ru.clevertec.exception.handling.starter.response.ErrorResponse;
 
@@ -12,7 +13,8 @@ import ru.clevertec.exception.handling.starter.response.ErrorResponse;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            ResourceNotFoundException e, HttpServletRequest request) {
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(e.getMessage())
@@ -20,6 +22,18 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(
+            AuthenticationException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED)
+                .message(e.getMessage())
+                .url(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
 }
