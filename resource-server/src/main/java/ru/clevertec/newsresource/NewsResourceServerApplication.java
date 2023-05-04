86a1b4229e074ef7a32c502b4ff4b969f2ff3f02
misha <mikhail.kutovenko@gmail.com>
2023-05-04 15:49:39 +0300

    Add @EnableFeignClients

--- resource-server/src/main/java/ru/clevertec/newsresource/NewsResourceServerApplication.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/NewsResourceServerApplication.java
@@ -3,8 +3,10 @@ package ru.clevertec.newsresource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 @ConfigurationPropertiesScan("ru.clevertec.newsresource")
 public class NewsResourceServerApplication {
 
