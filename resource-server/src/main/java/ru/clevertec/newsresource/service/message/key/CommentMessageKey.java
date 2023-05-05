c9e573519d315d20b432e98d82753676fed78ce7
misha <mikhail.kutovenko@gmail.com>
2023-05-05 16:04:55 +0300

    Add message keys

--- resource-server/src/main/java/ru/clevertec/newsresource/service/message/key/CommentMessageKey.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/message/key/CommentMessageKey.java
@@ -2,4 +2,8 @@ package ru.clevertec.newsresource.service.message.key;
 
 public class CommentMessageKey {
     public static final String COMMENT_NOT_FOUND_BY_ID = "comment.not-found.by-id";
+
+    public static final String UNABLE_TO_EDIT = "comment.unable-to-edit";
+
+    public static final String UNABLE_TO_DELETE = "comment.unable-to-delete";
 }
--- resource-server/src/main/java/ru/clevertec/newsresource/service/message/key/NewsMessageKey.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/message/key/NewsMessageKey.java
@@ -2,4 +2,9 @@ package ru.clevertec.newsresource.service.message.key;
 
 public class NewsMessageKey {
     public static final String NOT_FOUND_BY_ID = "news.not-found.by-id";
+
+    public static final String UNABLE_TO_EDIT = "news.unable-to-edit";
+
+    public static final String UNABLE_TO_DELETE = "news.unable-to-delete";
+
 }
