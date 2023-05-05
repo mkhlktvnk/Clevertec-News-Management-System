8d9446f2e183654bc3d4cafe2c45eb237e8e448d
misha <mikhail.kutovenko@gmail.com>
2023-05-05 19:53:39 +0300

    Update UserMessageKey

--- auth-server/src/main/java/ru/clevertec/auth/server/service/message/UserMessageKey.java
+++ auth-server/src/main/java/ru/clevertec/auth/server/service/message/UserMessageKey.java
@@ -1,5 +1,5 @@
 package ru.clevertec.auth.server.service.message;
 
 public class UserMessageKey {
-    public static final String USER_NOT_FOUND = "user.not-found.by-username";
+    public static final String USER_NOT_FOUND_BY_USERNAME = "user.not-found.by-username";
 }
