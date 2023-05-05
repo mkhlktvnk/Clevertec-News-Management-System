52f4e3e83497f2bf3c43ef51b2ab81fd3595cf21
misha <mikhail.kutovenko@gmail.com>
2023-05-06 00:25:55 +0300

    Refactor SecurityConfig

--- resource-server/src/main/java/ru/clevertec/newsresource/config/SecurityConfig.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,15 +24,18 @@ public class SecurityConfig {
     @SneakyThrows
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity.csrf().disable()
+                .cors().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/v0/news/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/v0/news/**").fullyAuthenticated()
+                .requestMatchers(HttpMethod.POST, "/api/v0/comments/**").hasAnyRole("ADMIN", "SUBSCRIBER")
                 .requestMatchers(HttpMethod.PATCH, "/api/v0/comments/**").hasAnyRole("ADMIN", "SUBSCRIBER")
                 .requestMatchers(HttpMethod.DELETE, "/api/v0/comments/**").hasAnyRole("ADMIN", "SUBSCRIBER")
+                .requestMatchers(HttpMethod.POST, "/api/v0/news/**").hasAnyRole("ADMIN", "JOURNALIST")
                 .requestMatchers(HttpMethod.PATCH, "/api/v0/news/**").hasAnyRole("ADMIN", "JOURNALIST")
                 .requestMatchers(HttpMethod.DELETE, "/api/v0/news/**").hasAnyRole("ADMIN", "JOURNALIST")
+                .anyRequest().permitAll()
                 .and()
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
