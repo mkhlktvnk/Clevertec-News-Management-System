b1e9c3fa5c18be67479bf03e53361677e5d4c348
misha <mikhail.kutovenko@gmail.com>
2023-05-05 22:38:57 +0300

    User renamed source

--- auth-server/src/main/java/ru/clevertec/auth/server/service/impl/UserServiceImpl.java
+++ auth-server/src/main/java/ru/clevertec/auth/server/service/impl/UserServiceImpl.java
@@ -11,7 +11,7 @@ import ru.clevertec.auth.server.entity.User;
 import ru.clevertec.auth.server.repository.RoleRepository;
 import ru.clevertec.auth.server.repository.UserRepository;
 import ru.clevertec.auth.server.service.UserService;
-import ru.clevertec.auth.server.service.message.MessageSource;
+import ru.clevertec.auth.server.service.message.MessagesSource;
 import ru.clevertec.auth.server.service.message.RoleMessageKey;
 import ru.clevertec.auth.server.service.message.UserMessageKey;
 import ru.clevertec.exception.handling.starter.exception.ResourceNotFoundException;
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-    private final MessageSource messageSource;
+    private final MessagesSource messagesSource;
 
     @Override
     @Transactional
@@ -33,7 +33,7 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         Role role = roleRepository.findByAuthority(RoleType.ROLE_SUBSCRIBER.name())
                         .orElseThrow(() -> new ResourceNotFoundException(
-                                messageSource.get(RoleMessageKey.ROLE_NOT_FOUND_BY_NAME,
+                                messagesSource.get(RoleMessageKey.ROLE_NOT_FOUND_BY_NAME,
                                         RoleType.ROLE_SUBSCRIBER.name())
                         ));
 
@@ -46,7 +46,7 @@ public class UserServiceImpl implements UserService {
     public UserDetails loadUserByUsername(String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResourceNotFoundException(
-                        messageSource.get(UserMessageKey.USER_NOT_FOUND_BY_USERNAME, username)
+                        messagesSource.get(UserMessageKey.USER_NOT_FOUND_BY_USERNAME, username)
                 ));
     }
 }
