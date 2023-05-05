5df1b12a588741987809f21ad2695e336c04f25f
misha <mikhail.kutovenko@gmail.com>
2023-05-05 22:36:16 +0300

    Rename MessagesSource

--- auth-server/src/main/java/ru/clevertec/auth/server/service/message/MessagesSource.java
+++ auth-server/src/main/java/ru/clevertec/auth/server/service/message/MessagesSource.java
@@ -1,14 +1,15 @@
 package ru.clevertec.auth.server.service.message;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
-public class MessageSource {
-    private final org.springframework.context.MessageSource messageSource;
+public class MessagesSource {
+    private final MessageSource messageSource;
 
     public String get(String key, Object... args) {
         return messageSource.getMessage(key, args, Locale.getDefault());
