cba6db2315526644a270696e8981e60286b2625a
misha <mikhail.kutovenko@gmail.com>
2023-05-05 15:57:53 +0300

    Add CommentPermissionService

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/impl/CommentPermissionService.java
@@ -0,0 +1,20 @@
+package ru.clevertec.newsresource.service.impl;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import ru.clevertec.newsresource.entity.Comment;
+import ru.clevertec.newsresource.service.PermissionService;
+
+@Service
+public class CommentPermissionService implements PermissionService<User, Comment> {
+
+    @Override
+    public boolean userHasPermissionToEditResource(User user, Comment resource) {
+        boolean isAdmin = user.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
+        boolean isCommentBelongsToUser = resource.getUsername().equals(user.getUsername());
+
+        return isAdmin || isCommentBelongsToUser;
+    }
+
+}
