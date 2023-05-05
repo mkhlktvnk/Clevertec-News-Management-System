505ce94e1f80b4560f88ddd35d9edd9d293e03ca
misha <mikhail.kutovenko@gmail.com>
2023-05-05 16:38:14 +0300

    Update SecurityConfig

--- resource-server/src/main/java/ru/clevertec/newsresource/config/SecurityConfig.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ru.clevertec.newsresource.security.entry.point.AuthEntryPoint;
 import ru.clevertec.newsresource.security.filter.AuthFilter;
 
 @Configuration
