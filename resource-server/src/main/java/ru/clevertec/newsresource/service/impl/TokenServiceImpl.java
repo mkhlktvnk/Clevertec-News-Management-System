d97ff57daead5e45c252f41e0f16a9e1d5e79f98
misha <mikhail.kutovenko@gmail.com>
2023-05-05 22:55:24 +0300

    Add TokenServiceImpl

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/service/impl/TokenServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.clevertec.newsresource.service.impl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import ru.clevertec.newsresource.service.TokenService;
+
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+    @Value("${jwt.publicKey}")
+    private RSAPublicKey publicKey;
+
+    @Override
+    public User getUserInfoFromToken(String token) {
+        String username = getUsernameFromToken(token);
+        List<SimpleGrantedAuthority> authorities = getRolesFromToken(token);
+
+        return new User(username, "", authorities);
+    }
+
+    private String getUsernameFromToken(String token) {
+        Claims claims = getClaimsFromToken(token);
+        return claims.get("username").toString();
+    }
+
+    private List<SimpleGrantedAuthority> getRolesFromToken(String token) {
+        Claims claims = getClaimsFromToken(token);
+        List<String> roles = (List<String>) claims.get("roles");
+        return roles.stream().map(SimpleGrantedAuthority::new).toList();
+    }
+
+    private Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(publicKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
