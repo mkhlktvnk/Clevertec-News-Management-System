0cf3728d890b25d22004959342a4263f91374b64
misha <mikhail.kutovenko@gmail.com>
2023-05-05 19:53:26 +0300

    Update UserServiceImpl

--- auth-server/src/main/java/ru/clevertec/auth/server/service/impl/UserServiceImpl.java
+++ auth-server/src/main/java/ru/clevertec/auth/server/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package ru.clevertec.auth.server.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +11,11 @@ import ru.clevertec.auth.server.entity.User;
 import ru.clevertec.auth.server.repository.RoleRepository;
 import ru.clevertec.auth.server.repository.UserRepository;
 import ru.clevertec.auth.server.service.UserService;
+import ru.clevertec.auth.server.service.message.MessageSource;
+import ru.clevertec.auth.server.service.message.RoleMessageKey;
+import ru.clevertec.auth.server.service.message.UserMessageKey;
+import ru.clevertec.exception.handling.starter.exception.ResourceNotFoundException;
 
-import java.util.ArrayList;
 import java.util.Collections;
 
 @Service
@@ -23,17 +25,18 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final MessageSource messageSource;
 
     @Override
     @Transactional
     public User saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         Role role = roleRepository.findByAuthority(RoleType.ROLE_SUBSCRIBER.name())
-                .orElseGet(() -> {
-                    Role authority = new Role();
-                    authority.setAuthority(RoleType.ROLE_SUBSCRIBER.name());
-                    return roleRepository.save(authority);
-                });
+                        .orElseThrow(() -> new ResourceNotFoundException(
+                                messageSource.get(RoleMessageKey.ROLE_NOT_FOUND_BY_NAME,
+                                        RoleType.ROLE_SUBSCRIBER.name())
+                        ));
+
         user.setAuthorities(Collections.singletonList(role));
 
         return userRepository.save(user);
@@ -42,6 +45,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDetails loadUserByUsername(String username) {
         return userRepository.findByUsername(username)
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        messageSource.get(UserMessageKey.USER_NOT_FOUND_BY_USERNAME, username)
+                ));
     }
 }
