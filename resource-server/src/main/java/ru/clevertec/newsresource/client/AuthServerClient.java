4a0889dcaef1782f436514401b2c827fa7dec063
misha <mikhail.kutovenko@gmail.com>
2023-05-05 22:56:41 +0300

    Update AuthServerClient

--- resource-server/src/main/java/ru/clevertec/newsresource/client/AuthServerClient.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/client/AuthServerClient.java
@@ -1,15 +1,14 @@
 package ru.clevertec.newsresource.client;
 
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import ru.clevertec.newsresource.web.dto.AuthResponseDto;
 
 @FeignClient(value = "auth-server", url = "http://localhost:9000/")
 public interface AuthServerClient {
 
-    @GetMapping("/validate")
-    AuthResponseDto validate(@RequestHeader("Authorization") String token);
+    @GetMapping("/auth/validate")
+    ResponseEntity<?> validate(@RequestHeader("Authorization") String token);
 
 }
