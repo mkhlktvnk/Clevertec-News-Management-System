7a63cd53121d645c9378b079a8933c13c7552ea0
misha <mikhail.kutovenko@gmail.com>
2023-05-08 23:35:32 +0300

    Add AuthConstant

---
+++ resource-server/src/main/java/ru/clevertec/newsresource/security/constant/AuthConstant.java
@@ -0,0 +1,7 @@
+package ru.clevertec.newsresource.security.constant;
+
+public class AuthConstant {
+    public static final String AUTHORIZATION = "Authorization";
+
+    public static final String BEARER = "Bearer ";
+}
