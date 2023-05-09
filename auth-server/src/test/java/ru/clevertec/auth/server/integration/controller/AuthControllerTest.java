82e919861a273a2f5ff3dccc5c6cb27ec4bb62e1
misha <mikhail.kutovenko@gmail.com>
2023-05-10 01:16:13 +0300

    Add AuthControllerTest

---
+++ auth-server/src/test/java/ru/clevertec/auth/server/integration/controller/AuthControllerTest.java
@@ -0,0 +1,44 @@
+package ru.clevertec.auth.server.integration.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.clevertec.auth.server.integration.BaseIntegrationTest;
+import ru.clevertec.auth.server.model.AuthRequest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+class AuthControllerTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @SneakyThrows
+    void registerReturnAuthResponseWithNotNullIdAndCorrectUsername() {
+        AuthRequest request = AuthRequest.builder()
+                .username("user123")
+                .password("password123")
+                .build();
+
+        mockMvc.perform(post("/auth/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.username").value(request.getUsername()));
+    }
+}
