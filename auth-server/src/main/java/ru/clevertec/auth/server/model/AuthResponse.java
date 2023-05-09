8e4011c753f47e850710736e28ac9e4556d68ca5
misha <mikhail.kutovenko@gmail.com>
2023-05-09 20:24:47 +0300

    Add @Builder

--- auth-server/src/main/java/ru/clevertec/auth/server/model/AuthResponse.java
+++ auth-server/src/main/java/ru/clevertec/auth/server/model/AuthResponse.java
@@ -2,12 +2,16 @@ package ru.clevertec.auth.server.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
 
