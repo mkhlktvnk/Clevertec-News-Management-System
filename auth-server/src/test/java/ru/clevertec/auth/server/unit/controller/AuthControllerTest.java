83bc8ea0471255676efb7a1deb4b718a5db8953b
misha <mikhail.kutovenko@gmail.com>
2023-05-09 20:24:07 +0300

    Add AuthControllerTest

---
+++ auth-server/src/test/java/ru/clevertec/auth/server/unit/controller/AuthControllerTest.java
@@ -0,0 +1,88 @@
+package ru.clevertec.auth.server.unit.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.clevertec.auth.server.controller.AuthController;
+import ru.clevertec.auth.server.model.AuthRequest;
+import ru.clevertec.auth.server.model.AuthResponse;
+import ru.clevertec.auth.server.service.AuthService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+
+    @Mock
+    private AuthService authService;
+
+    @InjectMocks
+    private AuthController authController;
+
+    @Test
+    void authenticateShouldReturnTokenWithOkStatus() {
+        String username = "user123";
+        String token = "test-token";
+        AuthRequest authRequest = AuthRequest.builder()
+                .username(username)
+                .password("password123")
+                .build();
+        doReturn(token).when(authService).authenticate(authRequest);
+
+        ResponseEntity<String> response = authController.authenticate(authRequest);
+
+        verify(authService).authenticate(authRequest);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(token);
+    }
+
+    @Test
+    void registerShouldReturnAuthResponseWithOkStatus() {
+        String username = "user123";
+        AuthRequest authRequest = AuthRequest.builder()
+                .username(username)
+                .password("password123")
+                .build();
+        AuthResponse authResponse = AuthResponse.builder()
+                .id(1L)
+                .username(username)
+                .build();
+        doReturn(authResponse).when(authService).register(authRequest);
+
+        ResponseEntity<AuthResponse> response = authController.register(authRequest);
+
+        verify(authService).register(authRequest);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getId()).isNotNull();
+        assertThat(response.getBody().getUsername()).isEqualTo(authRequest.getUsername());
+    }
+
+    @Test
+    void validateShouldReturnOkStatus() {
+        String token = "Bearer test-token";
+        doReturn(true).when(authService).isTokenValid(token);
+
+        ResponseEntity<?> response = authController.validate(token);
+
+        verify(authService).isTokenValid(token);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void validateTokenShouldReturnUnauthorizedStatus() {
+        String token = "Bearer test-token";
+        doReturn(false).when(authService).isTokenValid(token);
+
+        ResponseEntity<?> response = authController.validate(token);
+
+        verify(authService).isTokenValid(token);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+}
