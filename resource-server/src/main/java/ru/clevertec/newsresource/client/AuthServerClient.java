e326e2daacf0b3e57f73f4dbaa8ea1998544b798
misha <mikhail.kutovenko@gmail.com>
2023-05-10 10:07:00 +0300

    Update AuthServerClient

--- resource-server/src/main/java/ru/clevertec/newsresource/client/AuthServerClient.java
+++ resource-server/src/main/java/ru/clevertec/newsresource/client/AuthServerClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "auth-server", url = "http://localhost:9000/")
+@FeignClient(value = "auth-server", url = "http://auth-server:9000/")
 public interface AuthServerClient {
 
     @GetMapping("/auth/validate")
