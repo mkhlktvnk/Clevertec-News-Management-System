aed14bcd2fc412186813fc3d1b85bef7b88bd8b8
misha <mikhail.kutovenko@gmail.com>
2023-05-10 18:11:56 +0300

    Add db.changelog-data.sql

---
+++ resource-server/src/main/resources/db/changelog/scripts/db.changelog-data.sql
@@ -0,0 +1,87 @@
+INSERT INTO news (username, title, text)
+VALUES ('user1', 'New study shows benefits of regular exercise',
+        'A new study published in the Journal of Health and Fitness suggests that regular exercise can significantly improve overall health and well-being.');
+INSERT INTO news (username, title, text)
+VALUES ('user2', 'Experts predict surge in renewable energy investment',
+        'Leading energy experts are predicting a significant increase in investment in renewable energy projects over the next decade.');
+INSERT INTO news (username, title, text)
+VALUES ('user3', 'Global pandemic highlights importance of digital connectivity',
+        'The COVID-19 pandemic has emphasized the crucial role of digital connectivity in keeping people connected and businesses running.');
+INSERT INTO news (username, title, text)
+VALUES ('user4', 'New artificial intelligence technology promises to revolutionize healthcare',
+        'A new AI-based diagnostic tool developed by a team of researchers could dramatically improve the accuracy and speed of disease diagnosis.');
+INSERT INTO news (username, title, text)
+VALUES ('user5', 'Scientists discover new species of dinosaur',
+        'Paleontologists have unearthed fossils of a previously unknown species of dinosaur that roamed the earth millions of years ago.');
+INSERT INTO news (username, title, text)
+VALUES ('user6', 'New research reveals potential risks of artificial sweeteners',
+        'A recent study has found that consuming large quantities of artificial sweeteners could have negative health effects, including an increased risk of type 2 diabetes.');
+INSERT INTO news (username, title, text)
+VALUES ('user7', 'Experts warn of potential food shortages due to climate change',
+        'A report by leading climate scientists warns that global warming could have devastating impacts on agriculture, potentially leading to widespread food shortages in the coming decades.');
+INSERT INTO news (username, title, text)
+VALUES ('user8', 'New study suggests link between air pollution and cognitive decline',
+        'A study conducted by researchers at a leading university has found a correlation between exposure to air pollution and a decline in cognitive function, particularly in older adults.');
+INSERT INTO news (username, title, text)
+VALUES ('user9', 'NASA announces plans for manned mission to Mars',
+        'NASA has unveiled plans for a manned mission to Mars, with a target date set for the mid-2030s. The mission aims to explore the Red Planet and search for signs of extraterrestrial life.');
+INSERT INTO news (username, title, text)
+VALUES ('user10', 'New breakthrough in quantum computing could revolutionize cryptography',
+        'Scientists at a leading research institution have made a major breakthrough in quantum computing that could pave the way for new and more secure forms of cryptography.');
+INSERT INTO news (username, title, text)
+VALUES ('user11', 'Global efforts to reduce plastic waste gain momentum',
+        'Governments and organizations around the world are ramping up efforts to reduce plastic waste and promote recycling, in response to growing concerns about the impact of plastics on the environment.');
+INSERT INTO news (username, title, text)
+VALUES ('user12', 'New study sheds light on causes of depression',
+        'A new study has uncovered a possible link between inflammation and depression, providing new insights into the biological mechanisms that underlie the condition.');
+INSERT INTO news (username, title, text)
+VALUES ('user13', 'Researchers develop new treatment for Alzheimer’s disease',
+        'A team of scientists has developed a new drug that could potentially slow or even halt the progression of Alzheimer’s disease.');
+INSERT INTO news (username, title, text)
+VALUES ('user14', 'New report highlights growing inequality in education',
+        'A report by a leading education charity has highlighted the growing inequality in access to quality education, particularly in low-income communities.');
+
+INSERT INTO comments (username, text, news_id)
+VALUES ('user5', 'Wow, this is incredible news!', 1);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user2', 'I couldnt agree more. This is great!', 1);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user7', 'Finally some good news!', 1);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user3', 'This is a major breakthrough in renewable energy research.', 2);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user8', 'Hopefully this will lead to a cleaner and more sustainable future.', 2);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user4', 'AI technology is advancing so quickly, its amazing!', 4);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user10', 'I cant wait to see how this breakthrough is applied in the real world.', 10);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user6', 'I always suspected that artificial sweeteners were bad for you.', 6);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user12', 'This research is a big step forward in our understanding of depression.', 12);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user11', 'Its about time we start taking action on plastic waste!', 11);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user1', 'Regular exercise is one of the best things you can do for your health.', 1);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user9', 'A manned mission to Mars would be a truly historic achievement.', 9);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user5', 'I cant believe weve discovered a new species of dinosaur!', 5);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user8', 'This study highlights the importance of addressing air pollution.', 8);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user2', 'Im glad to see that efforts to reduce plastic waste are gaining momentum.', 11);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user10', 'This breakthrough in quantum computing could have major implications for cybersecurity.', 10);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user3', 'The pandemic has definitely shown us how important digital connectivity is.', 3);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user11', 'Its great to see global leaders taking action on plastic waste.', 11);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user6', 'Ill definitely be cutting back on artificial sweeteners after reading this study.', 6);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user4', 'The potential of AI in healthcare is truly exciting!', 4);
+INSERT INTO comments (username, text, news_id)
+VALUES ('user7', 'We need to take action now to avoid potential food shortages in the future.', 7);
+
+
