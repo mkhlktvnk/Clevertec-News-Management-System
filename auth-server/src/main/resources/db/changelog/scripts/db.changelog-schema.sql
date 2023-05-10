4007e8f89e9095dce19ef687f5ff143cc5e68faf
misha <mikhail.kutovenko@gmail.com>
2023-05-10 19:27:31 +0300

    Add table existence check

--- auth-server/src/main/resources/db/changelog/scripts/db.changelog-schema.sql
+++ auth-server/src/main/resources/db/changelog/scripts/db.changelog-schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE roles
+CREATE TABLE IF NOT EXISTS roles
 (
     id        SERIAL PRIMARY KEY,
     authority VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id       SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE users_roles
+CREATE TABLE IF NOT EXISTS users_roles
 (
     user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
     role_id INTEGER REFERENCES roles (id) ON DELETE CASCADE,
