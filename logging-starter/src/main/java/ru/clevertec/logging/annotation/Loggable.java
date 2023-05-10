ac4e7be1e053e3c0e0f887694c69e790eff0c235
misha <mikhail.kutovenko@gmail.com>
2023-05-10 12:02:46 +0300

    Add javadoc for logging-starter

--- logging-starter/src/main/java/ru/clevertec/logging/annotation/Loggable.java
+++ logging-starter/src/main/java/ru/clevertec/logging/annotation/Loggable.java
@@ -5,7 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the annotated method should be logged when it is invoked.
+ * This annotation should be applied to methods.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Loggable {
 }
+
--- logging-starter/src/main/java/ru/clevertec/logging/aspect/LoggableAspect.java
+++ logging-starter/src/main/java/ru/clevertec/logging/aspect/LoggableAspect.java
@@ -9,6 +9,19 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Aspect that intercepts method calls and logs information about them.
+ * This aspect is designed to work with methods annotated with the {@link ru.clevertec.logging.annotation.Loggable}
+ * annotation.
+ *
+ * The aspect logs information before the method is called, after the method returns successfully, and after the method
+ * throws an exception.
+ *
+ * The logging is done using the SLF4J logging framework, and logs the method name, arguments, return value (if any),
+ * and exception (if thrown).
+ *
+ * @since (the version of the software this aspect was introduced in)
+ */
 @Aspect
 public class LoggableAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggableAspect.class);
